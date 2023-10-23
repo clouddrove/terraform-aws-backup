182cc62adc4836ced93d3b7f89789497788b0a03
theprashantyadav <theprashantyadav7@gmail.com>
2023-10-23 18:40:57 +0530

    feat: updated .gitingore tflint issue and fixed efs module

--- _example/main.tf
+++ _example/main.tf
@@ -1,18 +1,20 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = local.region
+}
+locals {
+  name        = "buckup"
+  environment = "test"
+  region      = "eu-west-1"
 }
 
 module "vpc" {
   source  = "clouddrove/vpc/aws"
   version = "2.0.0"
 
+  name            = "${local.name}-vpc"
+  environment     = local.environment
   enable_flow_log = false
-
-  name        = "vpc"
-  environment = "test"
-  label_order = ["name", "environment"]
-
-  cidr_block = "10.0.0.0/16"
+  cidr_block      = "10.0.0.0/16"
 }
 
 
@@ -21,29 +23,24 @@ module "subnets" {
   version = "2.0.0"
 
   nat_gateway_enabled = true
-
-  name        = "subnets"
-  environment = "test"
-  label_order = ["name", "environment"]
-
-  availability_zones = ["eu-west-1a", "eu-west-1b"]
-  vpc_id             = module.vpc.vpc_id
-  type               = "public-private"
-  igw_id             = module.vpc.igw_id
-  cidr_block         = module.vpc.vpc_cidr_block
-  ipv6_cidr_block    = module.vpc.ipv6_cidr_block
+  name                = "${local.name}-subnets"
+  environment         = local.environment
+  availability_zones  = ["eu-west-1a", "eu-west-1b"]
+  vpc_id              = module.vpc.vpc_id
+  type                = "public-private"
+  igw_id              = module.vpc.igw_id
+  cidr_block          = module.vpc.vpc_cidr_block
+  ipv6_cidr_block     = module.vpc.ipv6_cidr_block
 }
 
 
 module "ssh" {
-  source      = "clouddrove/security-group/aws"
-  version     = "2.0.0"
-  name        = "ssh"
-  environment = "test"
-  label_order = ["name", "environment"]
+  source  = "clouddrove/security-group/aws"
+  version = "2.0.0"
 
-  vpc_id = module.vpc.vpc_id
-
+  name        = "${local.name}-ssh"
+  environment = local.environment
+  vpc_id      = module.vpc.vpc_id
   new_sg_ingress_rules_with_cidr_blocks = [{
     rule_count  = 1
     from_port   = 22
@@ -55,29 +52,39 @@ module "ssh" {
 }
 
 
+#module "efs" {
+#  source  = "clouddrove/efs/aws"
+#  version = "2.0.0"
+#
+#  name               = "${local.name}-efs"
+#  environment        = local.environment
+#  creation_token     = ""
+#  availability_zones = ""
+
+#}
+
 module "efs" {
-  source  = "clouddrove/efs/aws"
-  version = "1.3.1"
+  source                    = "clouddrove/efs/aws"
+  name                      = "efs"
+  environment               = "test"
+  creation_token            = "changeme"
+  availability_zones        = ["eu-west-1a", "eu-west-1b"]
+    vpc_id             = module.vpc.vpc_id
+    subnets            = module.subnets.public_subnet_id
+    security_groups    = [module.ssh.security_group_id]
 
-  name        = "efs"
-  environment = "test"
-  label_order = ["name", "environment"]
-
-  creation_token     = ""
-  region             = ""
-  availability_zones = ["eu-west-1a", "eu-west-1b"]
-  vpc_id             = module.vpc.vpc_id
-  subnets            = module.subnets.public_subnet_id
-  security_groups    = [module.ssh.security_group_id]
+  replication_configuration_destination = {
+    region                 = "eu-west-2"
+    availability_zone_name = ["eu-west-2a", "eu-west-2b"]
+  }
 }
 
 module "kms_key" {
   source  = "clouddrove/kms/aws"
   version = "1.3.1"
 
-  name                    = "kms"
-  environment             = "test"
-  label_order             = ["environment", "name"]
+  name                    = "${local.name}-kms"
+  environment             = local.environment
   enabled                 = true
   description             = "KMS key for ec2"
   deletion_window_in_days = 7
@@ -105,10 +112,8 @@ data "aws_iam_policy_document" "kms" {
 module "backup" {
   source = "./.."
 
-  name        = "backup"
-  environment = "test"
-  label_order = ["name", "environment"]
-
+  name                  = local.name
+  environment           = local.environment
   enabled               = true
   vault_enabled         = true
   plan_enabled          = true
